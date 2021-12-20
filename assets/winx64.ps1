55016b664bf72c6ab9629e97cf496fcd24b397b6
whi~nyaan <whinyaan@noreply.com>
2021-12-20 22:56:42 +0800

    test

---
+++ assets/winx64.ps1
@@ -0,0 +1,7 @@
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install curl
+curl 'https://github.com/whinee/MangDL-test/releases/download/0.0.0-alpha.4/mangdl-winx64.zip' -o mangdl.zip
+tar -xf mangdl.zip -C C:\
+Remove-Item -Force mangdl.zip
+"Set-Alias -Name mangdl -Value C:\mangdl\mangdl.bat" >> $PROFILE.CurrentUserAllHosts
+. $PROFILE.CurrentUserAllHosts
