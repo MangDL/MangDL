ab54746d85baccaf67e769da8ebb55d0c9332f8e
whi~nyaan <whinyaan@noreply.com>
2021-12-20 23:35:10 +0800

    test

--- assets/winx86.ps1
+++ assets/winx86.ps1
@@ -1,6 +1,6 @@
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install curl
-curl 'https://github.com/whinee/MangDL-test/releases/download/0.0.0-alpha.4/mangdl-winx86.zip' -o mangdl.zip
+curl 'https://github.com/MangDL/MangDL/releases/download/0.0.0-alpha.4/mangdl-winx86.zip' -o mangdl.zip
 tar -xf mangdl.zip -C C:\
 Remove-Item -Force mangdl.zip
 "Set-Alias -Name mangdl -Value C:\mangdl\mangdl.bat" >> $PROFILE.CurrentUserAllHosts
