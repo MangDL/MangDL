5b157d196d5ab290780bfc4793c6e5fee351e015
whi~nyaan <whinyaan@noreply.com>
2022-01-06 00:53:42 +0800

    dont mind me, just testing on main, bump to `2.0.1-alpha.2`

--- assets/linux-dl.sh
+++ assets/linux-dl.sh
@@ -1,2 +1,2 @@
-curl -L -o mangdl.AppImage https://github.com/MangDL/MangDL/releases/download/2.0.1-alpha.1/mangdl-linux-x86_64.AppImage
+curl -L -o mangdl.AppImage https://github.com/MangDL/MangDL/releases/download/2.0.1-alpha.2/mangdl-linux-x86_64.AppImage
 chmod +x ./mangdl.AppImage
