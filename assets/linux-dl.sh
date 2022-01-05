8c6dc32fcafe69eb0ea84d362daf4e95d2cda608
whi~nyaan <whinyaan@noreply.com>
2022-01-06 01:08:42 +0800

    bruh, wrong bump, das y, bump to `2.0.2-alpha.3`

--- assets/linux-dl.sh
+++ assets/linux-dl.sh
@@ -1,2 +1,2 @@
-curl -L -o mangdl.AppImage https://github.com/MangDL/MangDL/releases/download/2.0.1-alpha.2/mangdl-linux-x86_64.AppImage
+curl -L -o mangdl.AppImage https://github.com/MangDL/MangDL/releases/download/2.0.2-alpha.3/mangdl-linux-x86_64.AppImage
 chmod +x ./mangdl.AppImage
--- assets/port-winx86.ps1
+++ assets/port-winx86.ps1
@@ -1,6 +1,6 @@
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y curl 7zip.install
-curl 'https://github.com/MangDL/MangDL/releases/download/2.0.1-alpha.2/mangdl-winx86.zip' -o mangdl.zip
+curl 'https://github.com/MangDL/MangDL/releases/download/2.0.2-alpha.3/mangdl-winx86.zip' -o mangdl.zip
 Remove-Item -Force -Recurse C:\mangdl
 tar -xf mangdl.zip
 Remove-Item -Force mangdl.zip
